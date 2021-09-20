a66492080387b233205966bf4ab895fd6f2a6821
Pavel <pavel.polikutin10@mail.ru>
2021-09-20 12:20:56 +0300

    init commit

---
+++ consoleappprog1/consoleappprog1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleappprog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
